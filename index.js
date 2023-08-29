d8640ba99fbf62a890287b4af4709d20e612b886
TimalkaK <timalkamk@gmail.com>
2023-08-29 21:54:08 +0100

    feat: tidied up code

--- index.js
+++ index.js
@@ -1,58 +1,11 @@
-const getRandomMove = () => {
-    const moves = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * moves.length);
-    return moves[randomIndex];
-  };
-  
-  const getOutcome = (moveOne, moveTwo) => {
-    if (moveOne === moveTwo) {
-      return "It's a draw!";
-    }
-  
-    if (
-      (moveOne === "scissors" && moveTwo === "paper") ||
-      (moveOne === "rock" && moveTwo === "scissors") ||
-      (moveOne === "paper" && moveTwo === "rock")
-    ) {
-      return "Player One wins!";
-    }
-  
-    return "Player Two wins!";
-  };
   
   // Removing elements (nodes) from the DOM
-  const resetGame = () => {
-    if (document.getElementById("outcome")) {
-      const outcome = document.body.lastChild;
-      document.body.removeChild(outcome);
-    }
-  };
-  
-  const playGame = () => {
-    resetGame();
-    const playerOneMove = getRandomMove();
-    const playerTwoMove = getRandomMove();
-    const outcome = getOutcome(playerOneMove, playerTwoMove);
-    updateDOM(playerOneMove, playerTwoMove, outcome);
-  };
-  
-  const updateDOM = (moveOne, moveTwo, outcome) => {
-    // TODO Implement this method to update the DOM
-    // There are some images you can use in the images directory
-    //console.log(outcome);
-    document.getElementById('player-one-move__img').src = `images/${moveOne}.png`;
-    document.getElementById('player-two-move__img').src = `images/${moveTwo}.png`;
-    document.getElementById("player-one-move__name").textContent = moveOne;
-    document.getElementById("player-two-move__name").textContent = moveTwo;
-    
-    document.getElementById("winner-outcome").textContent = outcome;
-  };
-
-  const checkPassword = () => {
-    getPassword();
-
-  };
-
+  // const resetGame = () => {
+  //   if (document.getElementById("outcome")) {
+  //     const outcome = document.body.lastChild;
+  //     document.body.removeChild(outcome);
+  //   }
+  // };
   
 
 const getPassword = () => {
@@ -103,15 +56,7 @@ const getPassword = () => {
   }else{
     progress.style.accentColor="green";
   }
-
-  console.log(input);
-
-  console.log(progress);
-
- }
-
-
-
+}
 
 const password = document.getElementById("password");
 password.addEventListener("input", getPassword);
